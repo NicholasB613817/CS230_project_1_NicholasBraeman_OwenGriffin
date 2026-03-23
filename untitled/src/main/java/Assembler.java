ab1f8af4e6cc011770a47a8ec138355b9e3b683e
owenb <owenbgriffin@gmail.com>
2026-03-23 11:18:34 -0400

    User can now enter up until F, "15."

--- untitled/src/main/java/Assembler.java
+++ untitled/src/main/java/Assembler.java
@@ -21,6 +21,50 @@ public class Assembler {
                 System.out.println("The binary code is 0100!");
                 System.out.println("D0 01 00 F1 FC 16");
                 break;
+            case "5":
+                System.out.println("The binary code is 0101!");
+                System.out.println("D0 01 01 F1 FC 16");
+                break;
+            case "6":
+                System.out.println("The binary code is 0110!");
+                System.out.println("D0 01 10 F1 FC 16");
+                break;
+            case "7":
+                System.out.println("The binary code is 0111!");
+                System.out.println("D0 01 11 F1 FC 16");
+                break;
+            case "8":
+                System.out.println("The binary code is 1000!");
+                System.out.println("D0 10 00 F1 FC 16");
+                break;
+            case "9":
+                System.out.println("The binary code is 1001!");
+                System.out.println("D0 10 01 F1 FC 16");
+                break;
+            case "A":
+                System.out.println("The binary code is 1010!");
+                System.out.println("D0 10 10 F1 FC 16");
+                break;
+            case "B":
+                System.out.println("The binary code is 1011!");
+                System.out.println("D0 10 11 F1 FC 16");
+                break;
+            case "C":
+                System.out.println("The binary code is 1100!");
+                System.out.println("D0 11 00 F1 FC 16");
+                break;
+            case "D":
+                System.out.println("The binary code is 1101!");
+                System.out.println("D0 11 01 F1 FC 16");
+                break;
+            case "E":
+                System.out.println("The binary code is 1110!");
+                System.out.println("D0 11 10 F1 FC 16");
+                break;
+            case "F":
+                System.out.println("The binary code is 1111!");
+                System.out.println("D0 11 11 F1 FC 16");
+                break;
         }
         return null;
     }
