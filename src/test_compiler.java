7e2b9d7423f064555c006f5555cd228e5f82877a
NicholasB613817 <nicholasbraeman@gmail.com>
2026-03-27 10:41:09 -0400

    I fallow 'Let's Build a Compiler! (part 1)' to help figure out how to do a compiler

--- src/test_compiler.java
+++ src/test_compiler.java
@@ -1,2 +1,31 @@
+import java.io.IOException;
+import java.text.ParseException;
+
 public class test_compiler {
+    private static char lookahead;
+
+    public static void main(String[] args) throws IOException {
+            lookahead = (char) System.in.read();
+            try {
+                expression();
+            } catch (ParseException e) {
+                System.out.println(e.getMessage());
+            }
+    }
+
+    private static void expression() throws ParseException {
+        System.out.println("mov $" + getNum() + ", %eax");
+    }
+
+    private static char getNum() throws ParseException {
+        if (!Character.isDigit(lookahead)) {
+            throw new ParseException("integer expected");
+        }
+        return lookahead;
+    }
+    static class ParseException extends Exception {
+        ParseException(String reason) {
+            super(reason);
+        }
+    }
 }
